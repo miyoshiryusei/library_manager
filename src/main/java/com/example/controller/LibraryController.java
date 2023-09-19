d5508bf02af36c9e48b98ce12b79ba8dcdf4b29e
Miyoshi Ryusei <miyoshiryusei@gmail.com>
2023-09-19 14:42:24 +0900

    DBに問題がありエラーとなっておりDB修正済み。 コードに修正点はなかったためlesson2実装完了のコミットのためLibraryControllerを一部コメントアウトにしている状況。 lesson3以降の実装はコメントアウトを解除して進める。

--- src/main/java/com/example/controller/LibraryController.java
+++ src/main/java/com/example/controller/LibraryController.java
@@ -1,14 +1,9 @@
 package com.example.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.example.entity.Library;
 import com.example.service.LibraryService;
 
 @Controller
@@ -22,10 +17,12 @@ public class LibraryController {
         this.libraryService = libraryService;
     }
 
+    /*
     @GetMapping
     public String index(Model model) {
         List<Library> libraries = this.libraryService.findAll();
         model.addAttribute("libraries", libraries);
         return "library/index";
     }
+    */
 }
